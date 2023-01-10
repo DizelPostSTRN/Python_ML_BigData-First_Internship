ead98314a2ea3b9b0926752bede7128200f113e3
smaiseyenka <ragno1@mail.ru>
2023-01-10 18:23:44 +0300

    ch04 v2

--- ch04_basics.py
+++ ch04_basics.py
@@ -1,6 +1,17 @@
+import math
+
+
 # Task 1
 def get_day_week(day, starting_dotw):
     return (starting_dotw + day - 2) % 7 + 1
 
 
 print(get_day_week(7, 3))
+
+
+# Task 2
+def count_digits(n):
+    return int(math.log10(n))+1
+
+
+print(count_digits(12345678))
