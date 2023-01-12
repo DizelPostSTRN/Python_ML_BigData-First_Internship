944f78cd77fbf2033fc4a1b0aff48a0375e60037
smaiseyenka <ragno1@mail.ru>
2023-01-12 18:03:40 +0300

    ch05 v2

--- ch05_collections.py
+++ ch05_collections.py
@@ -9,3 +9,15 @@ def remove_3_largest(lst):
 
 
 print(remove_3_largest([1, 2, 3, 1, 2, 3]))
+
+
+# Task2
+def get_unique_lists_num(one, two, tree, four):
+    return len(set(map(tuple, (one, two, tree, four))))
+
+
+one = [1, 2, 3]
+two = [1, 2, 3]
+tree = [3, 4, 5]
+four = [3, 2, 1]
+print(get_unique_lists_num(one, two, tree, four))
