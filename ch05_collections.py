6c7d48e9a3ea13a1f2495a19235735d1a4f2e41b
smaiseyenka <ragno1@mail.ru>
2023-01-12 18:04:23 +0300

    ch05 v3

--- ch05_collections.py
+++ ch05_collections.py
@@ -21,3 +21,13 @@ two = [1, 2, 3]
 tree = [3, 4, 5]
 four = [3, 2, 1]
 print(get_unique_lists_num(one, two, tree, four))
+
+
+# Task3
+def get_max_common(a, b):
+    return max(set(a) & set(b) | {0})
+
+
+a = [1, 2, 3, 4, 5]
+b = [0, 1, 3, 5, 7]
+print(get_max_common(a, b))
