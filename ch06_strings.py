884c76a25384bd95611b21c32b215f6b1b1d4ff2
smaiseyenka <ragno1@mail.ru>
2023-01-13 16:13:47 +0300

    ch06 v2

--- ch06_strings.py
+++ ch06_strings.py
@@ -4,3 +4,11 @@ def remove_dots_quotas(text):
 
 
 print(remove_dots_quotas('hello..." text'))
+
+
+# Task2
+def borded_string(s):
+    return "{2:{2}^{1}}\n{2} {0} {2}\n{2:{2}^{1}}".format(s, len(s) + 4, "#")
+
+
+print(borded_string('Armando'))
