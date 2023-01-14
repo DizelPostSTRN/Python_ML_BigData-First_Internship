67657643245e4e6ffb7771e986a64b13815a6de8
smaiseyenka <ragno1@mail.ru>
2023-01-14 18:27:07 +0300

    ch07 v2

--- ch07_dictionaries.py
+++ ch07_dictionaries.py
@@ -7,3 +7,12 @@ def swap_dict(d):
 
 
 print(swap_dict({1: 2, 3: 4, 5: 4, 7: 2, 9: 4}))
+
+
+# Task2
+def compact_dict(d):
+    new_dict = swap_dict(d)
+    return {v: k for k, v in new_dict.items()}
+
+
+print(compact_dict({1: 2, 3: 4, 5: 4, 7: 2, 9: 4}))
