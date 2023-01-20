4a8fb0363023e5777bf8d53866e269ead6484691
smaiseyenka <ragno1@mail.ru>
2023-01-20 14:07:56 +0300

    ch09 v3

--- ch09_functions.py
+++ ch09_functions.py
@@ -39,3 +39,29 @@ def get_text(name1, name2):
 
 
 print(get_text(name1="WORLD", name2="SPACE"))
+
+
+# Task3
+def fn(f, n):
+    if n < 1:
+        return lambda y: y
+
+    return lambda y: fn(f, n - 1)(f(y))
+
+
+f1 = fn(lambda x: "sin(%s)" % x, 5)
+f2 = fn(lambda x: sin(x), 5)
+print("%s = %f" % (f1("1"), f2(1)))
+print("%s = %f" % (f1("2"), f2(2)))
+print(fn(lambda x: sin(x), 0)(1000))
+
+
+def golden_ratio(n):
+    f = fn(lambda x: 1 + (1 / x) if x != 0 else 1, n + 1)
+    return f(0)
+
+
+print(golden_ratio(0))
+print(golden_ratio(1))
+print(golden_ratio(2))
+print(golden_ratio(100))
