0cc999df9ca74819606f7a8a11a604a626270471
smaiseyenka <ragno1@mail.ru>
2023-01-20 14:07:15 +0300

    ch09 v2

--- ch09_functions.py
+++ ch09_functions.py
@@ -7,3 +7,35 @@ def to_celsius(temperature_list):
 
 
 print(to_celsius([102.56, 97.7, 132.2]))
+
+
+# Task2
+def bold(f):
+    def wrapped(*args, **kwargs):
+        return "<b>" + f(*args, **kwargs) + "</b>"
+
+    return wrapped
+
+
+def italic(f):
+    def wrapped(*args, **kwargs):
+        return "<i>" + f(*args, **kwargs) + "</i>"
+
+    return wrapped
+
+
+def underline(f):
+    def wrapped(*args, **kwargs):
+        return "<u>" + f(*args, **kwargs) + "</u>"
+
+    return wrapped
+
+
+@bold
+@italic
+@underline
+def get_text(name1, name2):
+    return "hello " + name1 + " and " + name2
+
+
+print(get_text(name1="WORLD", name2="SPACE"))
