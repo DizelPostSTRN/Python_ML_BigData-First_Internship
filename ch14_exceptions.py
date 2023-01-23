ccda6b0d3af82dd168a60b2d51a069decb80f090
smaiseyenka <ragno1@mail.ru>
2023-01-23 14:19:47 +0300

    ch14 v3

--- ch14_exceptions.py
+++ ch14_exceptions.py
@@ -19,8 +19,14 @@ def retrieve_age(person):
             raise PersonTooOld(f"The age of person is {age}. Person too old")
         if age > 122:
             raise PersonNotAlive(f"The age of person is {age}. Today there are no people of this age")
-    except:
-        return None
+    except ValueError:
+        print("ValueError: Value not int")
+    except KeyError:
+        print("KeyError: Key of dict not age")
+    except PersonTooOld:
+        print(f"PersonTooOld: Person too old")
+    except PersonNotAlive:
+        print(f"PersonNotAlive: Today there are no people of this age")
 
 
 print(retrieve_age({"age": 45}))
