3a5778bd5662804bb74d9c6ca3f91b5c441e1fba
DizelPostSTRN <ragno1@mail.ru>
2023-02-13 18:08:56 +0300

    ch15 v3

--- ch15_parser_MP3.py
+++ ch15_parser_MP3.py
@@ -19,10 +19,10 @@ def parse_id3v2(filename: str = "") -> list:
         with open(filename, "rb") as file:
             id3_data = file.read()
             file_id = id3_data[:3]  # Получаем идентификатор файла (ID3)
-            if b'ID3' not in file_id:
+            if file_id != b'ID3':
                 raise NotMP3Error(f"File {filename} not MP3")  # Если файл не начинается с идентификатора ID3
             id3_ver = id3_data[3]  # Версия ID3v2
-            if 3 not in id3_ver and 4 not in id3_ver:
+            if id3_ver not in (3, 4):
                 raise VersionError(f"ID3V2 version of file {filename} not 3 or 4")  # Если версия не 3 и не 4
             id3_s_bytes = id3_data[6:10]  # Размер данных в байтовой записи
             # Переводим в int
@@ -31,16 +31,16 @@ def parse_id3v2(filename: str = "") -> list:
             id3_data = id3_data[10:10 + id3_size]
             while id3_data:
                 frame_id = id3_data[:4]  # Идентификатор кадра
-                if b'\x00\x00\x00\x00' in frame_id:  # Если идентификатора нет пропускаем итерацию
+                if frame_id == b'\x00\x00\x00\x00':  # Если идентификатора нет пропускаем итерацию
                     break
-                if 3 in id3_ver:  # Размер данных для версии 3
+                if id3_ver == 3:  # Размер данных для версии 3
                     frame_size = int.from_bytes(id3_data[4:8], 'big')
-                elif 4 in id3_ver:  # Размер данных для версии 4
+                elif id3_ver == 4:  # Размер данных для версии 4
                     frame_s_b = id3_data[4:8]
                     frame_size = (frame_s_b[0] << 21) + (frame_s_b[1] << 14) + (frame_s_b[2] << 7) + frame_s_b[3]
                 frame_data = id3_data[10:10 + frame_size]  # Данные кадра
                 frame_text_data = None
-                if 'T' in chr(frame_id[0]):  # Если идентификатор начинается с T, пытаемся преобразовать в текст
+                if chr(frame_id[0]) == 'T':  # Если идентификатор начинается с T, пытаемся преобразовать в текст
                     if frame_data[0] in encodings.keys():
                         frame_text_data = frame_data[1:].decode(encodings[frame_data[0]])
                 id3_data = id3_data[10 + frame_size:]  # Делаем срез массива с данными, удаляя обработанные
@@ -52,8 +52,8 @@ def parse_id3v2(filename: str = "") -> list:
                         frame_text_data
                     )
                 )  # Добавляем кортеж в список
-    except:
-        result = []
+    except (IOError, NotMP3Error, VersionError) as e:
+        print(f"{e.__class__.__name__}: {e}")
     return result
 
 
